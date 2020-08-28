f8c26d11b038d5d568e5ba798c52748309f32d46
William Donovan-Seid <wdstwo@live.com>
2020-08-28 11:16:16 -0700

    add topping price (2 each) to totalPrice and start working making everything pretty

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // BUSINESS LOGIC //
-// Pizza Stuff
+// Pizza Object
 
 function Pizza() {
   this.size;
@@ -9,6 +9,19 @@ function Pizza() {
 
 Pizza.prototype.addSize = function(size) {
   this.size = size;
+  switch(size) {
+    case "small":
+      this.totalPrice = 14;
+      break;
+    case "medium":
+      this.totalPrice = 16;
+      break;
+    case "large":
+      this.totalPrice = 18;
+      break;
+    case "x-large":
+      this.totalPrice = 22;
+  }
 }
 
 Pizza.prototype.addToppings = function() {
@@ -16,7 +29,7 @@ Pizza.prototype.addToppings = function() {
   for (i=0; i<allToppings.length; i++) {
     if (allToppings[i].checked) {
       this.toppings.push(allToppings[i].value);
-      this.totalPrice = this.totalPrice
+      this.totalPrice = this.totalPrice + 2;
     }
   }
 }
@@ -35,5 +48,9 @@ $(document).ready(function() {
 
     thePizza.addSize(pizzaSize);
     thePizza.addToppings();
+
+    let orderTotal = thePizza.totalPrice;
+    $("#recipt").show();
+    $("#order-total").text(orderTotal);
   });
 });
