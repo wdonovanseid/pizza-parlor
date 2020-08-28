ca2e445376eb1738ff778cf1b78d04712b518b68
William Donovan-Seid <wdstwo@live.com>
2020-08-28 09:39:03 -0700

    add better addToppings method (not tested yet), beginnings of user interface, and change some ids on html (userName -> user-name)

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
+// BUSINESS LOGIC //
+// Pizza Stuff
+
 function Pizza() {
   this.size = "";
   this.toppings = [];
+  this.totalPrice = 0;
 }
 
 Pizza.prototype.addSize = function(size) {
   this.size.concat(size);
 }
 
-Pizza.prototype.addTopping = function(topping) {
+Pizza.prototype.addToppings = function() {
+  let allToppings = document.forms[0];
+  for (i=0; i<allToppings.length; i++) {
+    if (allToppings[i].checked) {
+      this.toppings.push(allToppings[i]);
+      this.totalPrice = this.totalPrice + allToppings[i].val();
+    }
+  }
   this.toppings.push(topping);
 }
 
+// USER INTERFACE //
+
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    const userName = $("input#user-name").val();
+    const pizzaSize = $("select#pizza-size").val();
+    let 
+  });
+});
